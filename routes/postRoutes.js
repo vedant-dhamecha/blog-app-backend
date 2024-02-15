a54229939b17bb89f86047a6ebb475f7ef8ab1ad
vedant_dhamecha <vedantdhamecha9525@gmail.com>
2024-02-15 19:39:20 +0530

    delete endpoint

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
 const router = express.Router();
-import { createPost, updatePost } from "../conrollers/postControllers.js";
+import {
+    createPost,
+    deletePost,
+    updatePost,
+  } from "../conrollers/postControllers.js";
 import { authGuard, adminGuard } from "../middleware/authMiddleware.js";
 
 router.post("/", authGuard, adminGuard, createPost);
-router.put("/:slug", authGuard, adminGuard, updatePost);
+router
+  .route("/:slug")
+  .put(authGuard, adminGuard, updatePost)
+  .delete(authGuard, adminGuard, deletePost);
 
 export default router;
