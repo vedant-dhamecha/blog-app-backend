b49f976272dcbb8aa5063399dcc2722a9e813400
vedant_dhamecha <vedantdhamecha9525@gmail.com>
2024-03-08 23:26:32 +0530

    delete comments

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { createComment, updateComment } from "../conrollers/commentControllers.js";
+import { createComment, updateComment, deleteComment } from "../conrollers/commentControllers.js";
 import { authGuard } from "../middleware/authMiddleware";
 
 router.post("/", authGuard, createComment);
-router.put("/:commentId", authGuard, updateComment);
+router
+  .route("/:commentId")
+  .put(authGuard, updateComment)
+  .delete(authGuard, deleteComment);
 
 export default router;
